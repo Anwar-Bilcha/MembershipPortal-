95f159a0a7a523ee7a2c4b5bbf2c6177de47e7ec
Anwar-Bilcha <60840040+Anwar-Bilcha@users.noreply.github.com>
2021-11-20 20:52:09 +0300

    Update CorsPolicy.js

--- CorsPolicy.js
+++ CorsPolicy.js
@@ -1,3 +1,4 @@
 //This file contains information about remote clients allowed to use the MembershipPortal Web API app.
 //New allowed clients will be added as they subscribe to the service
 //
+Now allowed editting
